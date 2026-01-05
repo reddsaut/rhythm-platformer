b63f906bf94c8b3ab49a8fedcc625816d61d4a82
James McConnell <123115150+jdmcconne@users.noreply.github.com>
2026-01-05 13:14:28 -0600

    non stupid logic

--- src/entities/Player.lua
+++ src/entities/Player.lua
@@ -1,7 +1,7 @@
 local player = {}
 
 player.draw = function (x, y)
-        love.graphics.rectangle("line", x, y, 50, 100)
+        love.graphics.rectangle("line", player.x, player.y, 50, 100)
 end
 
 function player:init (x, y)
--- src/systems/DrawSystem.lua
+++ src/systems/DrawSystem.lua
@@ -3,7 +3,7 @@ local drawSystem = Tiny.processingSystem()
 drawSystem.filter = Tiny.requireAll("draw", "x", "y")
 
 function drawSystem:process(e)
-    e.draw(50, 50)
+    e.draw()
 end
 
 return drawSystem
