099736f53a9a1aea11da6e3473e8bafd9d13a442
reddsaut <osauter@macalester.edu>
2026-01-05 21:14:34 -0600

    collision changes

--- main.lua
+++ main.lua
@@ -4,20 +4,27 @@ Tiny = require "lib.tiny"
 local Player = require("src.entities.Player")
 local Light = require("src.entities.Light")
 local BeatDisplay = require("src.entities.BeatDisplay")
+local Platform = require("src.entities.Platform")
 
 local conductor = require("src.systems.Conductor")
 local drawSystem = require("src.systems.DrawSystem")
 local playerControlSystem = require("src.systems.PlayerControlSystem")
+local bumpSystem = require("src.systems.BumpSystem")
+
+local width, height, flags = love.window.getMode()
 
 local world = Tiny.world(
     conductor,
     drawSystem,
     playerControlSystem,
+    bumpSystem,
     Player(100,100),
     Light(50,50,4,{0,1,2,3}),
     Light(100,50,4,{1,3}),
     Light(150,50,8,{2}),
-    BeatDisplay(500,50,4)
+    BeatDisplay(500,50,4),
+    Platform(-width, height-100, width * 3, 100),
+    Platform(300, 250, 100, 50)
 )
 
 function love.load()
---
+++ src/systems/BumpSystem.lua
@@ -0,0 +1,28 @@
+local BumpSystem = Tiny.processingSystem()
+
+bump = require "lib.bump"
+local bumpWorld = bump.newWorld()
+
+BumpSystem.filter = Tiny.requireAll("x", "y", "width", "height")
+
+function BumpSystem:onAdd(e)
+	bumpWorld:add(e, e.x, e.y, e.width, e.height)
+end
+
+function BumpSystem:onRemove(e)
+	bumpWorld:remove(e)
+end
+
+function BumpSystem:process(e, dt)
+	if e.dx and e.dy then -- player :)
+		local actualX, actualY, cols, len = bumpWorld:check(e, e.x, e.y + 1) -- something under u
+		e.grounded = len > 0
+
+		local actualX, actualY, cols, len = bumpWorld:check(e, e.x, e.y - 1) -- something over u
+		if len > 0 and e.dy < 0 then e.dy = 0 end -- hit ur head
+
+		e.x, e.y, cols, len = bumpWorld:move(e, e.x + e.dx * dt, e.y + e.dy * dt) -- move player
+	end
+end
+
+return BumpSystem
